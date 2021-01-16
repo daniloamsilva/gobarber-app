9bc32154bde0221e46c2450809f5ea690775df4f
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2021-01-16 18:42:11 -0300

    Aulas: Densidade de pixel e Importando fontes externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
 };
 
 export default SignIn;
